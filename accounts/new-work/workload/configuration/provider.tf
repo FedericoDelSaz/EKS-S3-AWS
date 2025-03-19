06f024e1dfc51ec7b2cb9ccbdd2aef02a691b466
fedlope <federico.lopezdelsaz@novatec-gmbh.de>
2025-03-19 18:03:30 +0100

    kyverno

--- accounts/new-work/workload/configuration/provider.tf
+++ accounts/new-work/workload/configuration/provider.tf
@@ -23,3 +23,11 @@ provider "kubectl" {
   token                  = data.aws_eks_cluster_auth.eks.token
   load_config_file       = false
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
--- accounts/new-work/workload/configuration/versions.tf
+++ accounts/new-work/workload/configuration/versions.tf
@@ -8,6 +8,20 @@ terraform {
       source  = "alekc/kubectl"
       version = "2.0.4"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.30.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.16.1"
+    }
   }
   required_version = ">= 1.0"
 }
