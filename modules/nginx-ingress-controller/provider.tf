d42d89fc11b0830ecbf026a2b30da0d2e446d8c5
felopez <33026346+FedericoDelSaz@users.noreply.github.com>
2025-03-20 14:22:50 +0100

    provider fix
    Co-authored-by: fedlope <federico.lopezdelsaz@novatec-gmbh.de>

--- modules/nginx-ingress-controller/provider.tf
+++ modules/nginx-ingress-controller/provider.tf
@@ -1,7 +1,9 @@
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.16.1"
+    }
   }
+  required_version = ">= 1.0"
 }
